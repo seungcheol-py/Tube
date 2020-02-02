7810999c4df36bbf81d6afccbca53f5513262254
seungcheol-py <seung9412@naver.com>
2020-02-03 07:45:28 +0900

    After localsMiddleware

---
+++ Controllers/userController.js
@@ -0,0 +1,7 @@
+export const join = (req, res) => res.render("join");
+export const login = (req, res) => res.render("login");
+export const logout = (req, res) => res.render("logout");
+export const users = (res, req) => res.render("users");
+export const userDetail = (res, req) => res.render("userDetail");
+export const editProfile = (res, req) => res.render("editProfile");
+export const changePassword = (res, req) => res.render("changePassword");
