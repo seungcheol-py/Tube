23013e29897dc9c945fb15f37bf121ca6d02255e
seungcheol-py <seung9412@naver.com>
2020-02-04 04:06:08 +0900

    After Avatar

--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -1,7 +1,28 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+
+export const search = (req, res) => {
+  const searchTerm = req.query.term;
+  /* 
+  const {
+    query: { term: searchTerm }
+  } = req;
+  */
+  res.render("search", { pageTitle: "Search", searchTerm: searchTerm });
+};
+// searchTerm (왼쪽) 이라는 변수는 searchTerm(오른쪽)을 의미한다.
+// searchTerm (오른쪽)은 term에 할당된 이름이다.
+
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "Videos" });
+
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Wetube
 
 Clone coding
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
